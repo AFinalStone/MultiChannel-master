9cd52f2d000bd9cec94588bbcf963604caffbb76
shiyaolei <shiyaolei@yongrun.com>
2022-03-29 18:39:43 +0800

    add: 对项目配置进行迭代更新

--- app/src/main/java/com/shi/androidstudio/multichannel/MainActivity.java
+++ app/src/main/java/com/shi/androidstudio/multichannel/MainActivity.java
@@ -1,10 +1,10 @@
 package com.shi.androidstudio.multichannel;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
